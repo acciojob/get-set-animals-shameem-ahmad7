5f8cb063e1a1ba8d413b58cf3febb39da9764ada
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-25 17:57:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-//complete this code
-class Animal {}
+// Animal class
+class Animal {
+  constructor(species) {
+    this._species = species;  // _species is a private property
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to log the sound the animal makes
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
+
+// Cat class extending Animal
+class Cat extends Animal {
+  // Method specific to Cat
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Dog class extending Animal
+class Dog extends Animal {
+  // Method specific to Dog
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Example usage:
+
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
 
 // Do not change the code below this line
 window.Animal = Animal;
